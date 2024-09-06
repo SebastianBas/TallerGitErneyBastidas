67706f7bd56e1567a1010a27d7669f0c495dc416
Erney Sebastian Bastidas <fariceohb12@gmail.com>
2024-09-06 14:53:06 -0500

    Agregando boton traducir al ingles y su funcionalidad

--- script.js
+++ script.js
@@ -22,12 +22,38 @@ function numeroALetrasEs(num) {
     }
 }
 
+function numeroALetrasEn(num) {
+    const unidades = ['ZERO', 'ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE'];
+    const decenas = ['TEN', 'TWENTY', 'THIRTY', 'FORTY', 'FIFTY', 'SIXTY', 'SEVENTY', 'EIGHTY', 'NINETY'];
+    const centenas = ['ONE HUNDRED', 'TWO HUNDRED', 'THREE HUNDRED', 'FOUR HUNDRED', 'FIVE HUNDRED', 'SIX HUNDRED', 'SEVEN HUNDRED', 'EIGHT HUNDRED', 'NINE HUNDRED'];
+
+    if (num < 10) {
+        return unidades[num];
+    } else if (num < 100) {
+        let decena = Math.floor(num / 10);
+        let unidad = num % 10;
+        return unidad === 0 ? decenas[decena - 1] : decenas[decena - 1] + '-' + unidades[unidad];
+    } else if (num < 1000) {
+        let centena = Math.floor(num / 100);
+        let resto = num % 100;
+        if (resto === 0) {
+            return centenas[centena - 1];
+        } else {
+            return centenas[centena - 1] + ' ' + numeroALetrasEn(resto);
+        }
+    } else {
+        return 'Number out of range';
+    }
+}
+
 function traducirNumero(idioma) {
     const numero = parseInt(document.getElementById('numero').value);
     let resultado;
 
     if (idioma === 'es') {
         resultado = numeroALetrasEs(numero);
+    }else if (idioma === 'en') {
+        resultado = numeroALetrasEn(numero);
     }
 
     document.getElementById('modalResultado').innerText = resultado;
